c1feb8c381eb10268d1bb9571fac1563f7d61ab3
Wim Muskee <wimmuskee@gmail.com>
2016-12-03 11:32:56 +0100

    stats: add 2 github graphs

---
+++ stats/github-total-commits.plt
@@ -0,0 +1,23 @@
+if (!exists("plot_data")) plot_data='data.dat'
+set title "Github packages: monthly commit count"
+set xdata time
+set timefmt "%Y-%m"
+set datafile separator ","
+set terminal png size 480,400 enhanced truecolor font 'Verdana,9'
+set output "github-total-commits.png"
+set ylabel "Commits"
+set xlabel "Date"
+set xrange ["2015-08":"2016-11"]
+set pointsize 0.8
+set format x "%Y-%m"
+seconds_per_year=365*24*60*60
+set xtics 0.5*seconds_per_year
+set linetype 1 lc rgb "#beb8db" lw 1
+set linetype 2 lc rgb "#54487a" lw 1
+set border 11
+set xtics out
+set tics front
+set key below
+plot \
+  plot_data using 1:($2) title 'total' with filledcurves x1, \
+  plot_data using 1:($3) title 'non-gentoo.org' with filledcurves x1
---
+++ stats/github-unique-authors.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+source stats.conf
+
+# make tempfiles
+GITLOGS=$(mktemp)
+GITLOGS_PARSED=$(mktemp)
+PLOT_DATA=$(mktemp)
+
+if [ "$DEBUG" -eq "1" ] ; then
+	echo "gitlog: ${GITLOGS}"
+	echo "gitlog parsed: ${GITLOGS_PARSED}"
+	echo "plot data: ${PLOT_DATA}"
+fi
+
+# getting data
+cd ${GIT_GENTOO_PACKAGES} && git log --format='%ae,%ad' --date=short > ${GITLOGS}
+
+# parsing
+for line in $(cat /tmp/gitlogs.csv); do
+	email=$(echo ${line} | cut -d "," -f 1)
+	date=$(echo ${line} | cut -d "," -f 2)
+	yearmonth=${date:0:7}
+	echo "${yearmonth},${email}" >> ${GITLOGS_PARSED}
+done
+
+# making plot data
+for yearmonth in $(cat ${GITLOGS_PARSED} | cut -d "," -f 1 | sort -r | uniq); do
+	if  [ "${yearmonth}" == "2016-12" ]; then
+		continue
+	fi
+
+	if [ "${yearmonth}" == "2015-07" ]; then
+		break
+	fi
+
+	total=$(cat ${GITLOGS_PARSED} | grep ${yearmonth} | cut -d "," -f 2 | sort | uniq | wc -l)
+	gentoo=$(cat ${GITLOGS_PARSED} | grep ${yearmonth} | grep "gentoo.org" | cut -d "," -f 2 | sort | uniq | wc -l)
+	other=$(($total - $gentoo))
+	echo "${yearmonth},${total},${other}" >> ${PLOT_DATA}
+done
+
+# plot
+gnuplot -e "plot_data='${PLOT_DATA}'" github-unique-authors.plt
+
+# cleanup
+rm ${GITLOGS}
+rm ${GITLOGS_PARSED}
+rm ${PLOT_DATA}
